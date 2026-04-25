12eac1e2ca48d6b0bc8fc507b88633a372e6ad8d
Felipe Dev <brayan.torres-a@uniminuto.edu.co>
2026-04-24 21:20:06 -0500

    feat: implement Excel upload workflow

--- src/app/components/file-upload/file-upload.ts
+++ src/app/components/file-upload/file-upload.ts
@@ -1,4 +1,5 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { BroilerRecord } from '../../models/broiler.models';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,4 +7,27 @@
   templateUrl: './file-upload.html',
   styleUrl: './file-upload.css',
 })
-export class FileUpload {}
+export class FileUpload {
+  @Input() selectedFileName = '';
+  @Input() loading = false;
+  @Input() errorMessage = '';
+  @Input() successMessage = '';
+  @Input() previewRecords: BroilerRecord[] = [];
+
+  @Output() fileSelected = new EventEmitter<File>();
+  @Output() clearData = new EventEmitter<void>();
+  @Output() loadDemo = new EventEmitter<void>();
+  @Output() downloadTemplate = new EventEmitter<void>();
+
+  onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    this.fileSelected.emit(file);
+    input.value = '';
+  }
+}
